b875ce552ef7e84eb519554eb4ddd31e41c116d1
Vasanth Kumar <vasanthkumar.work@gmail.com>
2023-04-15 06:25:31 +0530

    Added getmapping in NotesController for Notes

--- src/main/java/com/vasanth/taskmanager/notes/NotesController.java
+++ src/main/java/com/vasanth/taskmanager/notes/NotesController.java
@@ -1,4 +1,21 @@
 package com.vasanth.taskmanager.notes;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("tasks/{taskId}/notes")
 public class NotesController {
+
+    @GetMapping
+    public String getNotes() {
+        return "these are all the notes for taskId:";
+    }
+
+    @GetMapping("{notesId}")
+    public String getNote(@PathVariable Long taskId, @PathVariable Long notesId) {
+        return "This is the note with notes id: " + notesId + "of task: " + taskId;
+    }
 }
